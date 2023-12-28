3b731e5be9dae32c79ab608e0cd4d841062f1eef
ademayowa <ademayowaadeniyi@gmail.com>
2023-12-28 23:08:18 +0100

    chore: added all jobs page

---
+++ app/(routes)/jobs/page.tsx
@@ -0,0 +1,11 @@
+import BaseLayout from '@/components/layouts/BaseLayout';
+
+type Props = {};
+
+export default function AllJobsPage({}: Props) {
+  return (
+    <BaseLayout>
+      <h1 className='text-center mt-40 text-lg'>Work in progress...</h1>
+    </BaseLayout>
+  );
+}
