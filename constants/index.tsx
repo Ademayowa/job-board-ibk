cc98def231ccd1a96bf513f73a19baa0ff6820ca
Mayowa <30834433+Ademayowa@users.noreply.github.com>
2024-05-14 01:25:24 -0700

    chore: removed home link

--- constants/index.tsx
+++ constants/index.tsx
@@ -1,9 +1,9 @@
 export const navLinks = [
-  {
-    id: 'home',
-    label: 'Home',
-    route: '/',
-  },
+  // {
+  //   id: 'home',
+  //   label: 'Home',
+  //   route: '/',
+  // },
   {
     id: 'jobs',
     label: 'Jobs',
